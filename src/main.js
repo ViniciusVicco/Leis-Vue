09f274da58e052461195e4f4eaacdc599d018579
vinicius.miranda <vinicius.miranda@tre-to.jus.br>
2020-01-14 16:09:05 -0300

    Instalando Vuetify

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from "vue-router";
 import router from "./router";
+import vuetify from './plugins/vuetify';
+import Vuetify from "vuetify";
 Vue.config.productionTip = false
-
+Vue.use(Vuetify)
 Vue.use(VueRouter);
 
 
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
